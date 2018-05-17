c40ef6f5bf7ba3922f603d5ee47e3c13e25f92fc
Nicole Houston <nicole.houston@cox.net>
2018-05-16 21:41:40 -0700

    Worked out what happens if timer runs out on the question and how to see if the correct answer has been chosen by the user

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,39 +1,73 @@
 // Create variables
-var counter = 0;
-var showQuestion;
-var time = 30;
+var showTimer;
+var time = 20;
 var timerRunning = false;
+var index = 0;
 
 // Create an array with question objects
 var questions = [
-    {q:"What is the name of Elrond's daughter?", a:["Eowyn", "Arwen", "Galdriel"]},
-    {q:"How old is Bilbo Baggins turning at the beginning of the 'Fellowship of the Ring'?", a:["111", "100", "185"]}
+    {q:"What is the name of Elrond's daughter?", a:["Eowyn", "Arwen", "Galdriel"], correctAnswer: "Arwen"},
+    {q:"How old is Bilbo Baggins turning at the beginning of the 'Fellowship of the Ring'?", a:["111", "121", "185"], correctAnswer: "111"}
 ];
 
-// Create an array with the correct answers
-var answers = ["Arwen", "111"];
+// Create functions
+function displayQuestion(){
+    $("#question").text(questions[index].q);
+    $("#answer1").val(questions[index].a[0]);
+    $("#answer1").text(questions[index].a[0]);
+    $("#answer2").val(questions[index].a[1]);
+    $("#answer2").text(questions[index].a[1]);
+    $("#answer3").val(questions[index].a[2]);
+    $("#answer3").text(questions[index].a[2]);
+}
 
+function stopTimer(){
+    timerRunning = false;
+    clearInterval(showTimer);
+}
+
+// Function that clears the question and answer choices to reveal the correct answer
+function revealAnswer(){
+    $("#question").text('');
+    $("#answer1").text('');
+    $("#answer2").text('');
+    $("#answer3").text('');
+}
+
+function countDown(){
+    $("#timeRemaining").text("Time remaining: " + time + " seconds");
+    if(time === 0){
+        stopTimer();
+        revealAnswer();
+        $("#revealAnswer").text("Out of time! The correct answer is: " + answers[index]);
+        index++;
+        setTimeout(startRound, 3000);
+    }
+    time--;
+}
+// Create a function that begins each round with a new question and starts the timer over again
+function startRound(){
+    displayQuestion();
+    time = 20;
+    $("#timeRemaining").text("Time remaining: " + time + " seconds");
+    if (!timerRunning){
+        showTimer = setInterval(countDown, 1000);
+        timerRunning = true;   
+    }
+
+}
+
+$(".answers").click(function(){
+    stopTimer();
+    if ($(this).val() === questions[index].correctAnswer){
+        alert("hello");
+    }
+    
+})
 
 // Function that begins the game when the user hits the start button
+//========================================================================================================
 $("#startButton").click(function(){
     $(".start").empty();
     startRound();
 })
-
-function nextQuestion(){
-    
-
-}
-
-function countDown(){
-    time--;
-    $("#timeRemaining").text("Time remaining: " + time + "seconds");
-}
-
-function startRound(){
-    if (!timerRunning){
-        showQuestion = setInterval(countDown, 1000);
-        timerRunning = true;
-    }
-
-}
